a1d54a7f074a9ad2ff347513e844677c3b479d73
Matthew <46129554+MatthewSummons@users.noreply.github.com>
2025-05-07 12:36:21 +0800

    Add table schemas

--- Assignments/A2/Tables/TableSchemas.sql
+++ Assignments/A2/Tables/TableSchemas.sql
@@ -3,13 +3,11 @@ CREATE USER 'u3035946760'@'localhost' IDENTIFIED BY 'C3358PASS';
 GRANT ALL ON TwentyFour.* TO 'u3035946760'@'localhost';
 
 CREATE TABLE UserInfo(
-    -- UserID     INT         NOT NULL AUTO_INCREMENT,
     username   VARCHAR(40) NOT NULL,
     passHash   CHAR(64)    NOT NULL,
     wins       INT         NOT NULL CHECK (wins >= 0),
     games      INT         NOT NULL,
     avgWinTime DOUBLE      NOT NULL CHECK (avgWinTime >= 0),
-    -- PRIMARY KEY (UserID),
     PRIMARY KEY (username),
     CHECK (games >= wins)  
 );
